ed1e10e3fe8a02b0ca52216bf466e3a1da65406d
Joshypeace <joshuachilapondwa2@gmail.com>
2026-02-01 17:39:50 +0200

    removed unsed nexf font google

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,10 @@
 import type React from "react"
 import type { Metadata } from "next"
-import { Geist, Geist_Mono } from "next/font/google"
+// import { Geist, Geist_Mono } from "next/font/google"
 import { Analytics } from "@vercel/analytics/next"
 import "./globals.css"
 import AuthSessionProvider from "@/components/providers/session-provider"
 
-const _geist = Geist({ subsets: ["latin"] })
-const _geistMono = Geist_Mono({ subsets: ["latin"] })
 
 export const metadata: Metadata = {
   title: "JayArts Multimedia - Photography, Videography & Design",
